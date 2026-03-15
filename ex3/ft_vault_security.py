67b65b45ac498f31c72f4777149a9cba0dfeeb2a
klucchin <klucchin@42student.nice.fr>
2026-03-15 16:04:07 +0100

    ex3 complete

---
+++ ex3/ft_vault_security.py
@@ -0,0 +1,34 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_vault_security.py                              :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: klucchin <klucchin@student.42.fr>         +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/03/15 15:46:02 by klucchin        #+#    #+#               #
+#  Updated: 2026/03/15 16:03:43 by klucchin        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===")
+
+print("Initiating secure vault access...")
+
+try:
+    print("Vault connection established with failsafe protocols\n")
+
+    with open("classified_data.txt", "r") as vault_file:
+        data = vault_file.read()
+        print("SECURE EXTRACTION:")
+        print(f"{data}\n")
+
+    with open("classified_vault.txt", "w") as archive_file:
+        archive_file.write("[CLASSIFIED] New security protocols archived")
+        print("SECURE PRESERVATION:")
+        print("[CLASSIFIED] New security protocols archived")
+
+    print("Vault automatically sealed upon completion\n")
+except FileNotFoundError:
+    print("Error: Vault source not found")
+finally:
+    print("All vault operations completed with maximum security.")
